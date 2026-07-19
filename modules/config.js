bcb665ac6804d683e16cea24d6aec9bfec3abe37
lovelatter <rurabby999@gmail.com>
2026-07-19 22:31:15 +0600

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -3,37 +3,15 @@ const fs = require('fs');
 
 const DB_FILE = path.join(__dirname, '../db.json');
 const UPLOADS_DIR = path.join(__dirname, '../uploads');
+const GITHUB_MUSIC_BASE_URL = "https://raw.githubusercontent.com/lovelatter/Love/main";
 
-if (!fs.existsSync(UPLOADS_DIR)) {
-    fs.mkdirSync(UPLOADS_DIR, { recursive: true });
-}
+if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 
-let db = {
-    linkDatabase: {},
-    userSessions: {},
-    totalLinksCreated: 0,
-    isMaintenanceMode: false,
-    bannedUsers: [],
-    registeredUsers: [],
-    usernameMap: {}
-};
-
-try {
-    if (fs.existsSync(DB_FILE)) {
-        db = { ...db, ...JSON.parse(fs.readFileSync(DB_FILE, 'utf8')) };
-    } else {
-        fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
-    }
-} catch (e) {
-    console.error(e);
-}
-
-const saveDB = () => {
-    try {
-        fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
-    } catch (e) {
-        console.error(e);
-    }
+const AUTOMATIC_MUSIC_MAPPING = {
+    love: `${GITHUB_MUSIC_BASE_URL}/love.mp3`,
+    birthday: `${GITHUB_MUSIC_BASE_URL}/bd.mp3`,
+    sorry: `${GITHUB_MUSIC_BASE_URL}/sorry.mp3`,
+    eid: `${GITHUB_MUSIC_BASE_URL}/eid.mp3`
 };
 
 const CATEGORY_CONFIGS = {
@@ -43,4 +21,13 @@ const CATEGORY_CONFIGS = {
     eid: { title: "Eid Mubarak", emojis: ["🤝", "🎇", "🫂"], question: "EID Mubarak 🌙", buttons: ["EID Mubarak"] }
 };
 
-module.exports = { db, saveDB, CATEGORY_CONFIGS, UPLOADS_DIR };
+let db = { linkDatabase: {}, userSessions: {}, totalLinksCreated: 0, isMaintenanceMode: false, bannedUsers: [], registeredUsers: [], usernameMap: {} };
+
+try {
+    if (fs.existsSync(DB_FILE)) db = { ...db, ...JSON.parse(fs.readFileSync(DB_FILE, 'utf8')) };
+    else fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+} catch (e) { console.error(e); }
+
+const saveDB = () => { try { fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2)); } catch (e) { console.error(e); } };
+
+module.exports = { AUTOMATIC_MUSIC_MAPPING, CATEGORY_CONFIGS, db, saveDB, DB_FILE, UPLOADS_DIR };
