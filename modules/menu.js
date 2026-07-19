f483cf7c9aec6d2d08706affba2ae46f1bcf6f56
lovelatter <rurabby999@gmail.com>
2026-07-20 02:36:54 +0600

    Create menu.js

---
+++ modules/menu.js
@@ -0,0 +1,15 @@
+const { Markup } = require('telegraf');
+const locale = require('./locale');
+const { db } = require('./utils');
+
+const sendMainMenu = (ctx, isEdit = false) => {
+    const fullName = `${ctx.from?.first_name || ""} ${ctx.from?.last_name || ""}`.trim() || "ব্যবহারকারী";
+    const keyboard = Markup.inlineKeyboard([
+        [Markup.button.callback(locale.btn_make, 'menu_makelink')],
+        [Markup.button.callback(locale.btn_feedback, 'menu_feedback'), Markup.button.callback(locale.btn_help, 'menu_help')]
+    ]);
+    if (isEdit) return ctx.editMessageText(locale.welcome(fullName), { reply_markup: keyboard.reply_markup, parse_mode: 'Markdown' }).catch(() => {});
+    return ctx.reply(locale.welcome(fullName), { reply_markup: keyboard.reply_markup, parse_mode: 'Markdown' }).catch(() => {});
+};
+
+module.exports = { sendMainMenu };
