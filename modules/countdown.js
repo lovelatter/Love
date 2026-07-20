a1657f12b949311359f19aaaeb6e4db745dfc676
lovelatter <rurabby999@gmail.com>
2026-07-20 06:19:08 +0600

    Update countdown.js

--- modules/countdown.js
+++ modules/countdown.js
@@ -1,35 +1,17 @@
 const { Markup } = require('telegraf');
 
-const COUNTDOWN_CONFIG = {
-    prompt: "⏰ টাইম কাউন্টডাউন সেট করুন।",
-    btn_no: "❌ No Countdown"
+const locale = {
+    prompt_countdown_ask: "⏰ টাইম কাউন্টডাউন সেট করুন।",
+    btn_no_countdown: "❌ No Countdown"
 };
 
 function showCountdownPrompt(ctx, db, saveDB, showImageUploadPrompt) {
-    ctx.editMessageText(COUNTDOWN_CONFIG.prompt, Markup.inlineKeyboard([
-        [Markup.button.callback(COUNTDOWN_CONFIG.btn_no, 'timer_no')],
+    ctx.editMessageText(locale.prompt_countdown_ask, Markup.inlineKeyboard([
+        [Markup.button.callback(locale.btn_no_countdown, 'timer_no')],
         [Markup.button.callback('🕒 ৩ মিনিট', 'set_time_3'), Markup.button.callback('🕒 ৫ মিনিট', 'set_time_5')],
-        [Markup.button.callback('🕒 ১০ মিনিট', 'set_time_10')],
+        [Markup.button.callback('🕒 ১০ মিনিট', 'set_time_10'), Markup.button.callback('🕒 ২০ মিনিট', 'set_time_20')],
         [Markup.button.callback("🔙 পেছনে যান", 'menu_makelink')]
     ]), { parse_mode: 'Markdown' }).catch(() => {});
 }
 
-function handleTimerNo(ctx, db, saveDB, showImageUploadPrompt) {
-    ctx.answerCbQuery();
-    if (!db.userSessions[ctx.chat.id]) db.userSessions[ctx.chat.id] = {};
-    db.userSessions[ctx.chat.id].pendingMinutes = null;
-    saveDB();
-    showImageUploadPrompt(ctx);
-}
-
-function handleSetTime(ctx, db, saveDB, showImageUploadPrompt) {
-    ctx.answerCbQuery();
-    const userId = ctx.chat.id;
-    if (!db.userSessions[userId]) db.userSessions[userId] = {};
-    const minutes = parseInt(ctx.match.input.replace('set_time_', ''), 10);
-    db.userSessions[userId].pendingMinutes = minutes;
-    saveDB();
-    showImageUploadPrompt(ctx);
-}
-
-module.exports = { showCountdownPrompt, handleTimerNo, handleSetTime, COUNTDOWN_CONFIG };
+module.exports = { showCountdownPrompt, locale };
