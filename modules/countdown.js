8cfd982b7e42010bc8b3f1b44d7bf88586a96fb9
lovelatter <rurabby999@gmail.com>
2026-07-25 02:50:47 +0600

    Update countdown.js

--- modules/countdown.js
+++ modules/countdown.js
@@ -14,4 +14,23 @@ function showCountdownPrompt(ctx, db, saveDB, showImageUploadPrompt) {
     ]), { parse_mode: 'Markdown' }).catch(() => {});
 }
 
-module.exports = { showCountdownPrompt, locale };
+function setupCountdownActions(bot, db, saveDB, showMusicUploadPrompt) {
+    bot.action('timer_no', async (ctx) => { 
+        ctx.answerCbQuery(); 
+        if (!db.userSessions[ctx.chat.id]) db.userSessions[ctx.chat.id] = {};
+        db.userSessions[ctx.chat.id].pendingMinutes = null; 
+        await saveDB();
+        showMusicUploadPrompt(ctx, db, saveDB, null); 
+    });
+
+    bot.action(/^set_time_/, async (ctx) => {
+        ctx.answerCbQuery();
+        const userId = ctx.chat.id;
+        if (!db.userSessions[userId]) db.userSessions[userId] = {};
+        db.userSessions[userId].pendingMinutes = parseInt(ctx.match.input.replace('set_time_', ''), 10);
+        await saveDB();
+        showMusicUploadPrompt(ctx, db, saveDB, null);
+    });
+}
+
+module.exports = { showCountdownPrompt, setupCountdownActions, locale };
