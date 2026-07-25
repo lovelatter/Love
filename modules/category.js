e0ca27d81b7b420600b24961307188f68a096c21
lovelatter <rurabby999@gmail.com>
2026-07-25 20:56:23 +0600

    Rename CATEGORY_CONFIGS to cat_config

--- modules/category.js
+++ modules/category.js
@@ -1,4 +1,4 @@
-const CATEGORY_CONFIGS = {
+const cat_config = {
     love: { title: "আমার মনের কিছু কথা", emojis: ["❤️", "💖", "💕"], question: "Do you love me? 🥺", buttons: ["Yes", "No"], label: "❤️ প্রেমের চিঠি (Love)" },
     birthday: { title: "Happy Birthday", emojis: ["🎈", "🎉", "🎊"], question: "Are you happy? 😊", buttons: ["Yes", "No"], label: "🎂 জন্মদিনের শুভেচ্ছা (Birthday)" },
     sorry: { title: "I'm Sorry", emojis: ["😭", "😞", "😥"], question: "Do you forgive me? 🥺", buttons: ["Yes", "No"], label: "🥺 দুঃখ প্রকাশ (Sorry)" },
@@ -7,10 +7,10 @@ const CATEGORY_CONFIGS = {
 
 const localeCategories = {
     choose_cat: "✨ কোন ক্যাটাগরির লিঙ্ক বানাতে চান?",
-    cat_love: CATEGORY_CONFIGS.love.label,
-    cat_birthday: CATEGORY_CONFIGS.birthday.label,
-    cat_sorry: CATEGORY_CONFIGS.sorry.label,
-    cat_eid: CATEGORY_CONFIGS.eid.label
+    cat_love: cat_config.love.label,
+    cat_birthday: cat_config.birthday.label,
+    cat_sorry: cat_config.sorry.label,
+    cat_eid: cat_config.eid.label
 };
 
-module.exports = { CATEGORY_CONFIGS, localeCategories };
+module.exports = { cat_config, localeCategories };
