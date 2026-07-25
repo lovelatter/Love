520c5880d625b596eb7ead5b97e69f374e86be28
lovelatter <rurabby999@gmail.com>
2026-07-25 22:36:25 +0600

    Delete modules/category.js

--- modules/category.js
+++
@@ -1,8 +0,0 @@
-const cat_config = {
-    love: { title: "আমার মনের কিছু কথা", emojis: ["❤️", "💖", "💕"], question: "Do you love me? 🥺", buttons: ["Yes", "No"] },
-    birthday: { title: "Happy Birthday", emojis: ["🎈", "🎉", "🎊"], question: "Are you happy? 😊", buttons: ["Yes", "No"] },
-    sorry: { title: "I'm Sorry", emojis: ["😭", "😞", "😥"], question: "Do you forgive me? 🥺", buttons: ["Yes", "No"] },
-    eid: { title: "Eid Mubarak", emojis: ["🤝", "🎇", "🫂"], question: "EID Mubarak 🌙", buttons: ["EID Mubarak"] }
-};
-
-module.exports = { cat_config };
