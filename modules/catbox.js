e626d77a8556dddf968fe42e80650fa7a372c4f0
lovelatter <rurabby999@gmail.com>
2026-07-27 00:51:33 +0600

    Update catbox.js

--- modules/catbox.js
+++ modules/catbox.js
@@ -1,7 +1,7 @@
 const FormData = require('form-data');
 const axios = require('axios');
 
-async function uploadToCatbox(fileUrl, fileExtension) {
+async function uploadToTelegraph(fileUrl, fileExtension) {
     try {
         const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
         
@@ -10,13 +10,12 @@ async function uploadToCatbox(fileUrl, fileExtension) {
         }
 
         const form = new FormData();
-        form.append('reqtype', 'fileupload');
         
-        form.append('fileToUpload', Buffer.from(response.data), {
+        form.append('file', Buffer.from(response.data), {
             filename: `file_${Date.now()}.${fileExtension}`
         });
 
-        const uploadResponse = await axios.post('https://catbox.moe/user/api.php', form, {
+        const uploadResponse = await axios.post('https://telegra.ph/upload', form, {
             headers: {
                 ...form.getHeaders()
             },
@@ -24,17 +23,17 @@ async function uploadToCatbox(fileUrl, fileExtension) {
             maxBodyLength: Infinity
         });
 
-        if (uploadResponse.status === 200 && typeof uploadResponse.data === 'string' && uploadResponse.data.startsWith('http')) {
-            return uploadResponse.data.trim();
+        if (uploadResponse.status === 200 && Array.isArray(uploadResponse.data) && uploadResponse.data[0] && uploadResponse.data[0].src) {
+            return 'https://telegra.ph' + uploadResponse.data[0].src;
         } else {
-            console.error('Catbox Response Error:', uploadResponse.data);
+            console.error('Telegraph Response Error:', uploadResponse.data);
             return null;
         }
 
     } catch (error) {
-        console.error('Catbox Upload Error:', error.message);
+        console.error('Telegraph Upload Error:', error.message);
         return null;
     }
 }
 
-module.exports = { uploadToCatbox };
+module.exports = { uploadToTelegraph };
