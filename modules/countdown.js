fc9bebab8ed2100c63d85a20643d3e1cf01636ed
lovelatter <rurabby999@gmail.com>
2026-07-27 03:36:59 +0600

    Update countdown.js

--- modules/countdown.js
+++ modules/countdown.js
@@ -3,9 +3,8 @@ const { Markup } = require('telegraf');
 function showCountdownPrompt(ctx, db, saveDB, showImageUploadPrompt) {
     ctx.editMessageText("⏰ কাউন্টডাউন টাইমার সেট করুন।", Markup.inlineKeyboard([
         [Markup.button.callback("❌ কাউন্টডাউন ছাড়া", 'timer_no')],
-        [Markup.button.callback('🕒 ২ মিনিট', 'set_time_2'), Markup.button.callback('🕒 ৫ মিনিট', 'set_time_5')],
-        [Markup.button.callback('🕒 ১০ মিনিট', 'set_time_10'), Markup.button.callback('🕒 ২০ মিনিট', 'set_time_20')],
-        [Markup.button.callback("🔙 Back", 'menu_makelink')]
+        [Markup.button.callback('🕒 ১ মিনিট', 'set_time_1'), Markup.button.callback('🕒 ২ মিনিট', 'set_time_2')],
+        [Markup.button.callback('🕒 ৫ মিনিট', 'set_time_5'), Markup.button.callback('🕒 ১০ মিনিট', 'set_time_10')]
     ]), { parse_mode: 'Markdown' }).catch(() => {});
 }
 
