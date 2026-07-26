4b268923134c71517b1252b89992df6385a375c9
lovelatter <rurabby999@gmail.com>
2026-07-26 09:42:30 +0600

    Update link.js

--- modules/link.js
+++ modules/link.js
@@ -64,8 +64,8 @@ async function processFinalLinkCreation(ctx, letterText, db, saveDB, bot, ADMIN_
 🔗 Main Link: ${finalGeneratedUrl}`;
 
     ADMIN_IDS.forEach(id => bot.telegram.sendMessage(id, adminNotificationText, Markup.inlineKeyboard([
-        [Markup.button.callback("btn1 Answer & Msg", `view_ans_msg_${uniqueId}`), Markup.button.callback("btn2 Visitor Info", `view_vi_${uniqueId}`)],
-        [Markup.button.callback("btn3 Link off", `adm_off_link_${uniqueId}`), Markup.button.callback("btn4 Ban user", `adm_ban_creator_${uniqueId}`)]
+        [Markup.button.callback("👁️‍🗨️ Answer & Msg", `view_ans_msg_${uniqueId}`), Markup.button.callback("👁️‍🗨️ Visitor Info", `view_vi_${uniqueId}`)],
+        [Markup.button.callback("❌ Link off", `adm_off_link_${uniqueId}`), Markup.button.callback("🚫 Ban user", `adm_ban_creator_${uniqueId}`)]
     ])).catch(() => {}));
 }
 
