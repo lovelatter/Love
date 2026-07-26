8a088c582ffd9b45056105d3a7701b6090694060
lovelatter <rurabby999@gmail.com>
2026-07-27 01:32:47 +0600

    Update photo.js

--- modules/photo.js
+++ modules/photo.js
@@ -1,5 +1,5 @@
 const { Markup } = require('telegraf');
-const { uploadToCatbox } = require('./catbox');
+const { uploadToTelegraph } = require('./catbox');
 
 const img_msg = {
     img_ask: "📸 ছবি দিতে চাইলে ছবিটি এখানে আপলোড করুন।"
